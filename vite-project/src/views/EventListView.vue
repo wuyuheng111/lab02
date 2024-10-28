56e31659ee217066d9f12f1942d6569bcf004f9b
wuyuheng <wyhdzy@outlook.com>
2024-10-28 21:12:05 +0800

    1

---
+++ vite-project/src/views/EventListView.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import EventCard from '@/components/EventCard.vue'
+import type { Event } from '@/type'
+import { ref, onMounted } from 'vue'
+import EventService from '@/services/EventService'
+const events = ref<Event[]>(null)
+onMounted(() => {
+  EventService.getEvents()
+    .then((response) => {
+      events.value = response.data
+    })
+    .catch((error) => {
+      console.error('There was an error!', error)
+    })
+})
+
+</script>
+<template>
+  <h1>Events For Good</h1>
+  <!-- new element -->
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
+
